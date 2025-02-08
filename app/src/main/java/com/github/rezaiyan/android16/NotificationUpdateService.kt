759790b6739bfc79b66aefc17a00ec12b0b000bc
ali <alirezaiyann@gmail.com>
2025-02-08 21:41:53 +0100

    Refactor: Remove unused imports in NotificationUpdateService
    Removed unused imports of `Handler` and `Looper` from `NotificationUpdateService.kt`.

--- app/src/main/java/com/github/rezaiyan/android16/NotificationUpdateService.kt
+++ app/src/main/java/com/github/rezaiyan/android16/NotificationUpdateService.kt
@@ -4,9 +4,7 @@ import android.app.NotificationManager
 import android.app.Service
 import android.content.Context
 import android.content.Intent
-import android.os.Handler
 import android.os.IBinder
-import android.os.Looper
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
